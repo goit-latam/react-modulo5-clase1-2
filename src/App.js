a47018ba98fd60bb63a68cdb1741b5a11e702fd6
goit-latam <126070957+goit-latam@users.noreply.github.com>
2023-07-25 12:26:53 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,17 +1,15 @@
-import { BrowserRouter } from "react-router-dom";
+import Navbar from './modules/Navbar/Navbar';
 
-import Navbar from "./modules/Navbar/Navbar";
+import UserRoutes from './UserRoutes';
 
-import UserRoutes from "./UserRoutes";
-
-import "./shared/styles/styles.scss";
+import './shared/styles/styles.scss';
 
 function App() {
   return (
-    <BrowserRouter>
+    <>
       <Navbar />
       <UserRoutes />
-    </BrowserRouter>
+    </>
   );
 }
 

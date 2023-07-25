1937f8aea6326abf62268f0e5896b0c4473d5440
goit-latam <126070957+goit-latam@users.noreply.github.com>
2023-07-25 12:12:54 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import App from './App';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter basename="/react-modulo5-clase1-2/">
+    <BrowserRouter basename="/react-modulo5-clase1-2">
       <App />
     </BrowserRouter>
   </React.StrictMode>
